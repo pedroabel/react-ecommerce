2ea52206405e0e68fd568a16634ff657c3274d0f
voidPedro <pedrooabell@gmail.com>
2023-03-02 01:29:16 -0300

    Component Product

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,22 @@ import FooterBanner from "../components/FooterBanner";
 const Home = ({ products, bannerData }) => (
   <div>
     <Banner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="justify-center">
+      <div class="mx-auto max-w-md text-center">
+        <h2 class="text-2xl font-bold sm:text-3xl">Our featured Aroma Range</h2>
+        <p class="mt-4 text-base text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
+          faucibus massa dignissim tempus.
+        </p>
+      </div>
 
-    <div className="text-left pt-3 ">
-      <h2 className="text-3xl font-semibold">Em destaque</h2>
+      <div className="flex flex-wrap justify-center gap-4 mt-5 w-full py-4">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
     </div>
 
-    <div className="flex flex-wrap justify-center gap-4 mt-5 w-full py-4">
-      {products?.map((product) => (
-        <Product key={product._id} product={product} />
-      ))}
-    </div>
     {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
   </div>
 );
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,12 +5,13 @@ import {
   AiOutlinePlus,
   AiOutlineStar,
   AiFillStar,
+  AiOutlineShoppingCart,
 } from "react-icons/ai";
 import Product from "@/components/Product";
 import { useStateContext } from "@/context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
-  const { image, name, details, price } = product;
+  const { image, name, details, price, category } = product;
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
@@ -20,83 +21,91 @@ const ProductDetails = ({ product, products }) => {
     setShowCart(true);
   };
   return (
-    <div>
-      <div className="product-detail-container">
-        <div>
-          <div className="image-container">
+    <section className="py-28 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:place-items-center lg:py-20">
+      <div>
+        <div className="flex bg-orange-500 justify-center">
+          <img
+            src={urlFor(image && image[index])}
+            className="rounded-2xl border-purple-300 w-[300px] h-[300px] cursor-pointer"
+          />
+        </div>
+        <div className="flex gap-4 mt-5 justify-center py-2">
+          {image?.map((item, i) => (
             <img
-              src={urlFor(image && image[index])}
-              className="product-detail-image"
+              key={i}
+              src={urlFor(item)}
+              className={
+                i === index
+                  ? "rounded-lg w-[70px] h-[70px] cursor-pointer bg-slate-900"
+                  : "rounded-lg w-[70px] h-[70px] cursor-pointer"
+              }
+              onMouseEnter={() => setIndex(i)}
             />
-          </div>
-          <div className="small-images-container">
-            {image?.map((item, i) => (
-              <img
-                key={i}
-                src={urlFor(item)}
-                className={
-                  i === index ? "small-image selected-image" : "small-image"
-                }
-                onMouseEnter={() => setIndex(i)}
-              />
-            ))}
-          </div>
+          ))}
+        </div>
+      </div>
+
+      <article className="px-8 pb-10">
+        <h2 className="bg-slate-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10">
+          {category}
+        </h2>
+        <h1 className="text-slate-900 mb-10 font-bold text-3xl lg:text-4xl">
+          {name}
+        </h1>
+        <p className="text-slate-600 mb-10 leading-relaxed">{details}</p>
+
+        <div className="flex flex-wrap items-center justify-between lg:flex-col lg:items-start lg:gap-2">
+          <ul className="flex items-center gap-5">
+            <li className="text-slate-900 font-bold text-2xl">R$ {price}</li>
+            <li className="bg-orange-100 py-1 px-2 text-orange-400 tracking-wide text-sm font-bold inline-block rounded shadow">
+              10%
+            </li>
+          </ul>
+
+          <p className="text-slate-600 text-sm">
+            <s>R$ {price * (1 / 10)}</s>
+          </p>
         </div>
 
-        <div className="product-detail-desc">
-          <h1>{name}</h1>
-          <div className="reviews">
-            <div>
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiOutlineStar />
-            </div>
-            <p>(20)</p>
-          </div>
-          <h4>Details:</h4>
-          <p>{details}</p>
-          <p className="price">${price}</p>
-          <div className="quantity">
-            <h3>Quantity:</h3>
-            <p className="quantity-desc">
+        <div className="mt-10 lg:flex items-center justify-between gap-2">
+          <ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow lg:flex-1">
+            <li className="cursor-pointer">
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="minus">{qty}</span>
-              <span className="plus" onClick={incQty}>
-                <AiOutlinePlus />
-              </span>
-            </p>
-          </div>
-          <div className="buttons">
-            <button
-              type="button"
-              className="add-to-cart"
-              onClick={() => onAdd(product, qty)}
-            >
-              Add to Cart
-            </button>
-            <button type="button" className="buy-now" onClick={handleBuyNow}>
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
+            </li>
+            <li>{qty}</li>
+            <span className="plus" onClick={incQty}>
+              <AiOutlinePlus />
+            </span>
+          </ul>
 
-      <div className="maylike-products-wrapper">
-        <h2>You may also like</h2>
-        <div className="marquee">
-          <div className="maylike-products-container track">
-            {products.map((item) => (
-              <Product key={item._id} product={item} />
-            ))}
+          <div className="lg:flex-1 ">
+            <div className="m-1 p-2 ">
+              <button
+                className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full lg:mt-0 hover:bg-orange-600 transition-all duration-200"
+                onClick={() => onAdd(product, qty)}
+              >
+                <AiOutlineShoppingCart /> Add to cart
+              </button>
+              {/* <button
+                type="button"
+                className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full lg:mt-0 hover:bg-orange-600 transition-all duration-200"
+                onClick={handleBuyNow}
+              >
+                Buy Now
+              </button> */}
+            </div>
           </div>
         </div>
+      </article>
+
+      <div className="flex flex-wrap justify-center gap-4 mt-5 w-full py-4">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
